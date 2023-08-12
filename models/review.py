8ddff92c24feada7364b678e77a51d8df7c58b99
gokesolomon <gokehsolomon@gmail.com>
2023-08-12 15:20:38 -0700

    console

---
+++ models/review.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module That Define Review class."""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """Class representing a Review."""
+
+    place_id = ""
+    user_id = ""
+    text = ""
---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines module of the State class."""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Class representing a State."""
+    name = ""
